1e4f29caddbea1221067d3cc4325de3981db8d74
Joar Leth <joar.leth@gmail.com>
2021-10-06 16:45:55 +0200

    Initial Commit

---
+++ Streaming Demo/Streaming_DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  Streaming_DemoApp.swift
+//  Streaming Demo
+//
+//  Created by Joar Leth on 2021-10-06.
+//
+
+import SwiftUI
+
+@main
+struct Streaming_DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Streaming DemoUITests/Streaming_DemoUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Streaming_DemoUITestsLaunchTests.swift
+//  Streaming DemoUITests
+//
+//  Created by Joar Leth on 2021-10-06.
+//
+
+import XCTest
+
+class Streaming_DemoUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}

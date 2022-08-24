e37533d5c552d0181736a6c907df8063143b3a0f
Sam Lindley <samlindley0@gmail.com>
2022-08-24 15:14:07 +0200

    update urls

--- App/Auth/AuthService.swift
+++ App/Auth/AuthService.swift
@@ -14,15 +14,14 @@ class AuthService {
     private var oidAgent: OIDExternalUserAgentIOS? = nil
     
     private var accessToken: String?
-    
     func auth() {
-        let authorizationEndpoint = URL(string: "https://dev-auth-keycloak.epidemicsite.com/auth/realms/epidemic-dev-realm/protocol/openid-connect/auth")!
-        let tokenEndpoint = URL(string: "https://dev-auth-keycloak.epidemicsite.com/auth/realms/epidemic-dev-realm/protocol/openid-connect/token")!
+        let authorizationEndpoint = URL(string: "https://login.epidemicsound.com/auth/realms/epidemic-users-eu-we1/protocol/openid-connect/auth")!
+        let tokenEndpoint = URL(string: "https://login.epidemicsound.com/auth/realms/epidemic-users-eu-we1/protocol/openid-connect/token")!
         let configuration = OIDServiceConfiguration(authorizationEndpoint: authorizationEndpoint,
                                                     tokenEndpoint: tokenEndpoint)
         
         let request = OIDAuthorizationRequest(configuration: configuration,
-                                              clientId: "papi-development-external-0af9cb57-0e5e-49ac-8bdf-f4cfcc127f73",
+                                              clientId: "papi-es-connect-prod-test-app-external-0de3b8ff-58b5-4439-aa2f-1961c1a0fc90",
                                               clientSecret: nil,
                                               scopes: nil,
                                               redirectURL: URL(string: "com.epidemicsound.partner-content-api-demo-ios:/oauthredirect")!,
